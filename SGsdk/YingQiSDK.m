ed8440174a8a780c3c7a7d4ebb2081edfeb3e47f
twksky <364116799@qq.com>
2017-07-22 16:31:45 +0800

    a

--- SGsdk/YingQiSDK.m
+++ SGsdk/YingQiSDK.m
@@ -54,7 +54,7 @@ static NSString *K_YingQi_HostName = @"http://dev.imyingqi.com/ChessWebServer/";
         
         NSDictionary *responseObject = [SGAppUtils JsonDataToObject:data];
         
-        if ([responseObject[@"state"] intValue] == 1) {
+        if ([responseObject[@"state"] boolValue]) {
             sB(responseObject);
         }else{
             fB(responseObject);
@@ -68,26 +68,106 @@ static NSString *K_YingQi_HostName = @"http://dev.imyingqi.com/ChessWebServer/";
 //手机登录注册-校验验证码 2
 +(void)YingQiSDKRequst_receiveCheckCode:(NSString *)number withCheckCode:(NSInteger)checkCode sB:(void (^)(NSDictionary * dic)) sB fB:(void (^)(NSDictionary * dic))fB{
     
+    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+    [dictionary setObject:number forKey:@"number"];
+    [dictionary setObject:@(checkCode) forKey:@"checkCode"];
+    
+    [[SGHTTPManager sharedManager] sg_AsyncPostRequestWithEncrypt:[NSString stringWithFormat:@"%@userPhone/receiveCheckCode",K_YingQi_HostName] content:dictionary successBlock:^(NSData *data) {
+        
+        NSDictionary *responseObject = [SGAppUtils JsonDataToObject:data];
+        
+        if ([responseObject[@"state"] boolValue]) {
+            sB(responseObject);
+        }else{
+            fB(responseObject);
+        }
+    } failedBlock:^(NSError *error) {
+        [[[UIAlertView alloc]initWithTitle:@"提示" message:@"网络请求失败" delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil] show];
+    }];
+    
 }
 
 //手机注册-用户信息 3
 +(void)YingQiSDKRequst_registerWithNumber:(NSString *)number withCheckCode:(NSInteger)checkCode withPwd:(NSString *)pwd sB:(void (^)(NSDictionary * dic)) sB fB:(void (^)(NSDictionary * dic))fB{
     
+    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+    [dictionary setObject:number forKey:@"number"];
+    [dictionary setObject:@(checkCode) forKey:@"checkCode"];
+    [dictionary setObject:pwd forKey:@"pwd"];
+    
+    [[SGHTTPManager sharedManager] sg_AsyncPostRequestWithEncrypt:[NSString stringWithFormat:@"%@userRegister/register",K_YingQi_HostName] content:dictionary successBlock:^(NSData *data) {
+        
+        NSDictionary *responseObject = [SGAppUtils JsonDataToObject:data];
+        
+        if ([responseObject[@"state"] boolValue]) {
+            sB(responseObject);
+        }else{
+            fB(responseObject);
+        }
+    } failedBlock:^(NSError *error) {
+        [[[UIAlertView alloc]initWithTitle:@"提示" message:@"网络请求失败" delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil] show];
+    }];
+    
 }
 
 //手机注册-校验手机注册状态（发验证码）4
 +(void)YingQiSDKRequst_checkPhoneRegWithNumber:(NSString *)number withCheckCode:(NSInteger)checkCode sB:(void (^)(NSDictionary * dic)) sB fB:(void (^)(NSDictionary * dic))fB{
+    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+    [dictionary setObject:number forKey:@"number"];
     
+    [[SGHTTPManager sharedManager] sg_AsyncPostRequestWithEncrypt:[NSString stringWithFormat:@"%@userRegister/checkPhoneReg",K_YingQi_HostName] content:dictionary successBlock:^(NSData *data) {
+        
+        NSDictionary *responseObject = [SGAppUtils JsonDataToObject:data];
+        
+        if ([responseObject[@"state"] boolValue]) {
+            sB(responseObject);
+        }else{
+            fB(responseObject);
+        }
+    } failedBlock:^(NSError *error) {
+        [[[UIAlertView alloc]initWithTitle:@"提示" message:@"网络请求失败" delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil] show];
+    }];
 }
 
 //手动输入账号、密码进行注册 5
 +(void)YingQiSDKRequst_registerAccountWithName:(NSString *)name withPwd:(NSString *)pwd sB:(void (^)(NSDictionary * dic)) sB fB:(void (^)(NSDictionary * dic))fB{
     
+    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+    [dictionary setObject:name forKey:@"name"];
+    [dictionary setObject:pwd forKey:@"pwd"];
+    
+    [[SGHTTPManager sharedManager] sg_AsyncPostRequestWithEncrypt:[NSString stringWithFormat:@"%@userRegister/registerAccount",K_YingQi_HostName] content:dictionary successBlock:^(NSData *data) {
+        
+        NSDictionary *responseObject = [SGAppUtils JsonDataToObject:data];
+        
+        if ([responseObject[@"state"] boolValue]) {
+            sB(responseObject);
+        }else{
+            fB(responseObject);
+        }
+    } failedBlock:^(NSError *error) {
+        [[[UIAlertView alloc]initWithTitle:@"提示" message:@"网络请求失败" delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil] show];
+    }];
 }
 
 //手动输入账号、用户登录-账号或手机号登录 6
 +(void)YingQiSDKRequst_loginWithNumberStr:(NSString *)numberStr withPwd:(NSString *)pwd sB:(void (^)(NSDictionary * dic)) sB fB:(void (^)(NSDictionary * dic))fB{
+    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+    [dictionary setObject:numberStr forKey:@"numberStr"];
+    [dictionary setObject:pwd forKey:@"pwd"];
     
+    [[SGHTTPManager sharedManager] sg_AsyncPostRequestWithEncrypt:[NSString stringWithFormat:@"%@userLogin/login",K_YingQi_HostName] content:dictionary successBlock:^(NSData *data) {
+        
+        NSDictionary *responseObject = [SGAppUtils JsonDataToObject:data];
+        
+        if ([responseObject[@"state"] boolValue]) {
+            sB(responseObject);
+        }else{
+            fB(responseObject);
+        }
+    } failedBlock:^(NSError *error) {
+        [[[UIAlertView alloc]initWithTitle:@"提示" message:@"网络请求失败" delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil] show];
+    }];
 }
 
 //用户登录-第三方登录 7
@@ -125,7 +205,24 @@ static NSString *K_YingQi_HostName = @"http://dev.imyingqi.com/ChessWebServer/";
 
 //用户手机号码找回密码 11
 +(void)YingQiSDKRequst_passwordWithNumber:(NSString *)number withCheckCode:(NSInteger)checkCode withPwd:(NSString *)pwd sB:(void (^)(NSDictionary * dic)) sB fB:(void (^)(NSDictionary * dic))fB{
+    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+    [dictionary setObject:@(1) forKey:@"type"];
+    [dictionary setObject:pwd forKey:@"pwd"];
+    [dictionary setObject:number forKey:@"number"];
+    [dictionary setObject:@(checkCode) forKey:@"checkCode"];
     
+    [[SGHTTPManager sharedManager] sg_AsyncPostRequestWithEncrypt:[NSString stringWithFormat:@"%@user.password",K_YingQi_HostName] content:dictionary successBlock:^(NSData *data) {
+        
+        NSDictionary *responseObject = [SGAppUtils JsonDataToObject:data];
+        
+        if ([responseObject[@"state"] boolValue]) {
+            sB(responseObject);
+        }else{
+            fB(responseObject);
+        }
+    } failedBlock:^(NSError *error) {
+        [[[UIAlertView alloc]initWithTitle:@"提示" message:@"网络请求失败" delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil] show];
+    }];
 }
 
 @end
